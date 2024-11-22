9d54ed05bb3863c5490479ac2898f9c4ba893944
Shivam Kumar <shivam.khantwal@sadhguru.org>
2024-11-22 16:45:21 +0530

    added multer

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { listProduct,addProduct,removeProduct,singleProduct } from '../controllers/productController';
+import upload from '../middleware/multer';
+
+const productRouter= express.Router();
+
+productRouter.post('/add',upload.fields([{name:'image1',maxCount:1}, {name:'image2',maxCount:1},{name:'image3',maxCount:1}  ,{name:'image4',maxCount:1}  ]), addProduct);
+
+productRouter.post('/remove',removeProduct);
+
+productRouter.post('/single',singleProduct);
+
+productRouter.get('/list',listProduct);
+
+
+export default productRouter;
